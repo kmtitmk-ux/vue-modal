4bbdffc5f021fd9995be3402cccebac5f678d061
Kazuki Matsui <>
2019-12-11 19:28:37 +0900

    モーダルのコンテンツを選択を連動させる

--- www/index.php
+++ www/index.php
@@ -5,17 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <div id="app">
-        <div class="d-flex justify-content-center mt-3 mb-3">
-            <span @click="click_show" class="btn btn-info">開く</span>
+    <div id="app" class="row justify-content-center col-12">
+        <div class="d-flex flex-wrap mt-3 mb-3 col-5">
+            <img-list
+                v-for="(item, index) in select"
+                v-bind:item="item"
+                v-bind:index="index"
+                v-bind:key="item.id"
+            ></img-list>
+            <div class="mt-4 w-100 d-flex justify-content-center">
+                <span @click="click_show" class="btn btn-info">開く</span>
+            </div>
         </div>
-        <modal-item v-if="show"></modal-item>
+        <modal-item
+            v-if="show"
+            v-bind:data="list"
+        ></modal-item>
     </div><!--//app-->
-
+    <script>
+        var list_array = [1, 2, 3, 4, 5]
+        var select_array = [1, 2]
+    </script>
     <script src="/js/app.js"></script>
 </body>
 </html>
--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,11 +1,59 @@
-const Vue = require('vue');
+require('bootstrap-css-only')
+const Vue = require('vue')
+
+Vue.component('img-list', {
+    props: ['item'],
+    template: `
+        <img class="w-25 h-auto" v-bind:src="'/images/' + item + '.jpg'">
+    `,
+})
+
+Vue.component('list', {
+    props: ['item'],
+    template: `
+        <div class="card w-25">
+            <img class="card-img" v-bind:src="'/images/' + item + '.jpg'" alt="">
+            <div class="card-img-overlay">
+                <input type="checkbox" v-bind:value="item" @change="select" v-model="checked">
+            </div>
+        </div>
+    `,
+    data: function (data) {
+        if (app.select.includes(data.item)) {
+            return {
+                checked: true,
+            }
+        } else {
+            return {
+                checked: false,
+            }
+        }
+    },
+    methods: {
+        select: function (e) {
+            if (e.target.checked) {
+                app.select.push(parseInt(e.target.value))
+            } else {
+                var index = app.select.indexOf(parseInt(e.target.value))
+                if (index > -1) {
+                    app.select.splice(index, 1)
+                }
+            }
+        }
+    }
+})
 
 Vue.component('modal-item', {
+    props: ['data'],
     template: `
         <div id="modal" @click="except_modal_close">
             <div class="wrap">
                 <span class="close" @click="modal_close">&times;</span>
-                <p class="text-center">モーダルで表示させるコンテンツ。</p>
+                <list　v-for="(item, index) in data"
+                    v-bind:item="item"
+                    v-bind:index="index"
+                    v-bind:key="item.id"
+                ></list>
             </div>
         </div>
     `,
@@ -15,19 +63,22 @@ Vue.component('modal-item', {
         },
         except_modal_close: function (e) {
             if (this.$el == e.target) {
-                app.$data.show = false;
+                app.$data.show = false
             }
         }
     }
 })
+
 const app = new Vue({
     el: '#app',
     data: {
-        show: false
+        show: false,
+        list: list_array,
+        select: select_array
     },
     methods: {
         click_show: function () {
-            this.show = true;
+            this.show = true
         }
     }
 })
